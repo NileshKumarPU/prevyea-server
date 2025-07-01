2f89ac389de34ecff6e70b13ec54303769354c15
Nilesh <codemate18@gmail.com>
2025-07-02 04:41:37 +0530

    cors issue update.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: "https://prevyea-server.vercel.app/",
+  origin: "https://prevyea-server.vercel.app",
   credentials: true
 }));
 app.use(cookieParser())
