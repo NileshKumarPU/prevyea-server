e635cea966aad23fd05f6b1dacabbfd827db1283
Nilesh <codemate18@gmail.com>
2025-07-06 15:35:21 +0530

    reset password controller

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -3,91 +3,128 @@ import { createSecretToken } from "../util/SecretToken.js";
 import User from "../Models/User.model.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { SendOTP, VerifyOTP,SendAck } from "../util/EmailOTP.js";
-
-
+import { SendOTP, VerifyOTP, SendAck } from "../util/EmailOTP.js";
 
 const prisma = new PrismaClient();
- 
-export const logOut = async(req,res)=>{
+
+export const logOut = async (req, res) => {
   try {
-    
     res.clearCookie("token", {
       secure: true,
       httpOnly: true,
       sameSite: "None",
       path: "/",
     });
-    res.status(200).json({success:true, message: "Logged out successfully" });
+    res.status(200).json({ success: true, message: "Logged out successfully" });
   } catch (error) {
-    res.status(501).json({success:false,message:"Logout failed"})
+    res.status(501).json({ success: false, message: "Logout failed" });
   }
-}
+};
 let expiresAt = 0;
-export const requestOTP = async (req, res) => {
+
+export const forgotpass_reqOTP = async (req, res) => {
   const { email } = req.body;
-   const existinguser = await prisma.user.findUnique({where:{email:email}})
-    if (existinguser) return res.json({success:false, message: "E-mail Already Registered" });
+  const existinguser = await prisma.user.findUnique({
+    where: { email: email },
+  });
+  if (!existinguser) {
+    return res.json({ success: false, message: "E-mail not registered." });
+  }
   try {
     await SendOTP(email);
     res.json({ success: true, message: "OTP sent" });
-    expiresAt= Date.now() + 5 * 60 * 1000;
+    expiresAt = Date.now() + 5 * 60 * 1000;
   } catch (error) {
     res.status(510).json({ success: false, message: "Failed to send OTP" });
   }
 };
 
-export const verifyOTP = async (req, res) => {
-    
+export const requestOTP = async (req, res) => {
+  const { email } = req.body;
+  const existinguser = await prisma.user.findUnique({
+    where: { email: email },
+  });
+  if (existinguser)
+    return res.json({ success: false, message: "E-mail Already Registered" });
   try {
-  const {  email,otp } = req.body;
- 
-  
-  if (Date.now() > expiresAt){ return res.json({success:false, message: 'OTP expired' });}
+    await SendOTP(email);
+    res.json({ success: true, message: "OTP sent" });
+    expiresAt = Date.now() + 5 * 60 * 1000;
+  } catch (error) {
+    res.status(510).json({ success: false, message: "Failed to send OTP" });
+  }
+};
 
+export const resetPass = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+    if (Date.now() > expiresAt) {
+      return res.json({ success: false, message: "OTP expired" });
+    }
     const authResult = await VerifyOTP(email, otp);
-    
-    
+
+    if (authResult) {
+      try {
+        
+        const newpassword = await bcrypt.hash(req.body.newpassword, 10);
+        const updatedUser = await prisma.user.update({
+          where: {email: email },
+          data: { password: newpassword },
+        });
+
+      } catch (error) {
+        return res.json({error:error});
+      }
+      res.json({ success: true, message: "Password Updated!" });
+    } else {
+      res.json({ success: false, message: "Invalid OTP" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: "Verification Error, Try Again" });
+  }
+};
+export const verifyOTP = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+    if (Date.now() > expiresAt) {
+      return res.json({ success: false, message: "OTP expired" });
+    }
+    const authResult = await VerifyOTP(email, otp);
     if (authResult) {
       res.json({ success: true, message: "OTP Verified" });
     } else {
       res.json({ success: false, message: "Invalid OTP" });
     }
   } catch (error) {
-    res.json({success:false,message:"Verification Error, Try Again"})
+    res.json({ success: false, message: "Verification Error, Try Again" });
   }
 };
 
-
-
-
 export const Signup = async (req, res, next) => {
   try {
-    const { username, email, fullname,admin } = req.body;
-    const password = await bcrypt.hash(req.body.password, 10)
-    const user = await prisma.user.create({ data: { username, email,fullname,password,admin } });
+    const { username, email, fullname, admin } = req.body;
+    const password = await bcrypt.hash(req.body.password, 10);
+    const user = await prisma.user.create({
+      data: { username, email, fullname, password, admin },
+    });
     const token = createSecretToken(user.id);
     res.cookie("token", token, {
       // withCredentials: true,
-      secure:true,
+      secure: true,
       httpOnly: true,
       sameSite: "None",
       path: "/",
-      maxAge: 7 * 24 * 60 * 60 * 1000 
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res
       .status(201)
       .json({ message: "User Signed Up Successfully", success: true, user });
-    SendAck(email,username);
+    SendAck(email, username);
     next();
   } catch (error) {
-    res.json({ message: "Oops! Try Again", success: false })
+    res.json({ message: "Oops! Try Again", success: false });
   }
 };
-
-
-
-
 
 export const Login = async (req, res, next) => {
   try {
@@ -96,34 +133,32 @@ export const Login = async (req, res, next) => {
       return res.json({ message: "All fields required!", success: false });
     }
 
-    const existinguser = await prisma.user.findUnique({where:{ email: email} });
- 
-    
+    const existinguser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
     if (!existinguser) {
       return res.json({ message: "User Not Registered!", success: false });
     }
-    
+
     const auth = await bcrypt.compare(password, existinguser.password);
-    
-    
-    
+
     if (!auth) {
       return res.json({
         message: "Incorrect Password or E-mail",
         success: false,
       });
     }
-    
-   
+
     const token = createSecretToken(existinguser.id);
     console.log(token);
-    
+
     res.cookie("token", token, {
       // withCredentials: true,
-      secure:true,
+      secure: true,
       httpOnly: true,
       sameSite: "None",
-      maxAge: 7 * 24 * 60 * 60 * 1000 ,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
     });
     res
@@ -131,6 +166,6 @@ export const Login = async (req, res, next) => {
       .json({ message: "User Logged in Successfully!", success: true });
     next();
   } catch (error) {
-    res.json({msg:"login error"})
+    res.json({ msg: "login error" });
   }
 };
--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,4 +1,4 @@
-import {Signup,Login,requestOTP,verifyOTP,logOut} from "../Controllers/AuthController.js"
+import {Signup,Login,requestOTP,verifyOTP,logOut,forgotpass_reqOTP, resetPass} from "../Controllers/AuthController.js"
 import { userVerification } from "../Middlewares/AuthMiddleware.js";
 import express from "express"
 const router = express.Router();
@@ -8,7 +8,8 @@ router.post("/login", Login);
 router.post("/logout",logOut);
 router.post("/otp",requestOTP);
 router.post("/verify",verifyOTP);
-
+router.post("/forgotpassreqotp",forgotpass_reqOTP)
+router.post("resetpass",resetPass);
 router.post("/", userVerification);
 
 export default router;
