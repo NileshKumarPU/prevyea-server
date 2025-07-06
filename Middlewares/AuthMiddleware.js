dc707c6e0d5c2bc38ce8cccaecc422d59bd5ccea
Nilesh <codemate18@gmail.com>
2025-07-06 09:40:05 +0530

    session verification

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,7 +1,9 @@
 import User from "../Models/User.model.js";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken"
+import { PrismaClient } from "../generated/prisma/index.js";
 
+const prisma = new PrismaClient();
 export const userVerification = (req,res) =>{
     const token = req.cookies.token;
     if(!token ){
@@ -11,7 +13,7 @@ export const userVerification = (req,res) =>{
     jwt.verify(token,process.env.TOKEN_KEY, async(err,decoded) =>{
         if(err) return res.json({status:false})
         else{
-            const user= await User.findById(decoded.id)
+            const user= await prisma.user.findUnique({where:{id:decoded.id}})
             if(user) {return res.json({status:true, user:user.username,admin:user.admin,email:user.email,fullname:user.fullname});}
                 else {return res.json({status:false});}
         }
